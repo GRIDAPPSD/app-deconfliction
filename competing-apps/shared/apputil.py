4c20271d9a779abf283819875238f81ce223f20f
garydblack <gary.black@pnnl.gov>
2023-02-28 12:27:29 -0800

    Migrated the code that queries and fills the various dictionaries like EnergyConsumers to the AppUtil class.  Even migrated commented out placeholder code into a function named getKitchenSink in case it ever proved useful and then I could delete it from the competing app code.

--- competing-apps/shared/apputil.py
+++ competing-apps/shared/apputil.py
@@ -10,7 +10,106 @@ from datetime import datetime
 class AppUtil:
   """ Class for competing app utility functions
   """
+
+  def getEnergyConsumers(sparql_mgr):
+    EnergyConsumers = {}
+    objs = sparql_mgr.obj_dict_export('EnergyConsumer')
+    print('Count of EnergyConsumers Dict: ' + str(len(objs)), flush=True)
+    for item in objs:
+      name = item['IdentifiedObject.name']
+      EnergyConsumers[name] = {}
+      # Shiva HACK to force battery charging...
+      #EnergyConsumers[name]['kW'] = 0.01*float(item['EnergyConsumer.p'])/1000.0
+      EnergyConsumers[name]['kW'] = float(item['EnergyConsumer.p'])/1000.0
+      EnergyConsumers[name]['kVar'] = float(item['EnergyConsumer.q'])/1000.0
+      #print('EnergyConsumer name: ' + name + ', kW: ' + str(EnergyConsumers[name]['kW']) + ', kVar: ' + str(EnergyConsumers[name]['kVar']), flush=True)
+
+    return EnergyConsumers
+
     
+  def getSynchronousMachines(sparql_mgr):
+    SynchronousMachines = {}
+    objs = sparql_mgr.obj_dict_export('SynchronousMachine')
+    print('Count of SynchronousMachines Dict: ' + str(len(objs)), flush=True)
+    for item in objs:
+      name = item['IdentifiedObject.name']
+      SynchronousMachines[name] = {}
+      SynchronousMachines[name]['kW'] = float(item['SynchronousMachine.p'])/1000.0
+      SynchronousMachines[name]['kVar'] = float(item['SynchronousMachine.q'])/1000.0
+      SynchronousMachines[name]['ratedS'] = float(item['SynchronousMachine.ratedS'])/1000.0
+      print('SynchronousMachine name: ' + name + ', kW: ' + str(round(SynchronousMachines[name]['kW'],4)) + ', kVar: ' + str(round(SynchronousMachines[name]['kVar'],4)), flush=True)
+
+    return SynchronousMachines
+
+
+  def getBatteries(sparql_mgr):
+    Batteries = {}
+    bindings = sparql_mgr.battery_query()
+    print('Count of Batteries: ' + str(len(bindings)), flush=True)
+    for obj in bindings:
+      name = obj['name']['value']
+      #bus = obj['bus']['value'].upper()
+      Batteries[name] = {}
+      Batteries[name]['ratedkW'] = float(obj['ratedS']['value'])/1000.0
+      Batteries[name]['ratedE'] = float(obj['ratedE']['value'])/1000.0
+      # Shiva HACK
+      Batteries[name]['SoC'] = 0.5
+      #Batteries[name]['SoC'] = float(obj['storedE']['value'])/float(obj['ratedE']['value'])
+      # eff_c and eff_d don't come from the query, but they are used throughout
+      # and this is a convenient point to assign them with query results
+      Batteries[name]['eff_c'] = 0.975 * 0.86
+      Batteries[name]['eff_d'] = 0.975 * 0.86
+      print('Battery name: ' + name + ', ratedE: ' + str(round(Batteries[name]['ratedE'],4)) + ', SoC: ' + str(round(Batteries[name]['SoC'],4)), flush=True)
+
+    return Batteries
+
+  def getSolarPVs(sparql_mgr):
+    SolarPVs = {}
+    bindings = sparql_mgr.pv_query()
+    print('Count of SolarPV: ' + str(len(bindings)), flush=True)
+    for obj in bindings:
+      name = obj['name']['value']
+      #bus = obj['bus']['value'].upper()
+      #ratedS = float(obj['ratedS']['value'])
+      #ratedU = float(obj['ratedU']['value'])
+      SolarPVs[name] = {}
+      SolarPVs[name]['kW'] = float(obj['p']['value'])/1000.0
+      SolarPVs[name]['kVar'] = float(obj['q']['value'])/1000.0
+      #print('SolarPV name: ' + name + ', kW: ' + str(SolarPVs[name]['kW']) + ', kVar: ' + str(SolarPVs[name]['kVar']), flush=True)
+
+    return SolarPVs
+
+
+  def getKitchenSink(sparql_mgr):
+    pass
+    # not needed now, but saving for possible future use
+
+    #objs = sparql_mgr.obj_meas_export('EnergyConsumer')
+    #print('Count of EnergyConsumers Meas: ' + str(len(objs)), flush=True)
+    #for item in objs:
+    #  print('EnergyConsumer: ' + str(item), flush=True)
+
+    #objs = sparql_mgr.obj_meas_export('PowerElectronicsConnection')
+    #print('Count of PowerElectronicsConnections Meas: ' + str(len(objs)), flush=True)
+    #for item in objs:
+    #  print('PowerElectronicsConnection: ' + str(item), flush=True)
+
+    #objs = sparql_mgr.obj_dict_export('LinearShuntCompensator')
+    #print('Count of LinearShuntCompensators Dict: ' + str(len(objs)), flush=True)
+    #for item in objs:
+    #  print('LinearShuntCompensator: ' + str(item), flush=True)
+
+    #objs = sparql_mgr.obj_meas_export('LinearShuntCompensator')
+    #print('Count of LinearShuntCompensators Meas: ' + str(len(objs)), flush=True)
+    #for item in objs:
+    #  print('LinearShuntCompensator: ' + str(item), flush=True)
+
+    #objs = sparql_mgr.obj_meas_export('SynchronousMachine')
+    #print('Count of SynchronousMachines Meas: ' + str(len(objs)), flush=True)
+    #for item in objs:
+    #  print('SynchronousMachine: ' + str(item), flush=True)
+
+
   def discharge_batteries(Batteries, deltaT):
     for name in Batteries:
       Batteries[name]['state'] = 'discharging'
