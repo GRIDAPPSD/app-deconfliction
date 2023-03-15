5ee772990c2dc5dfb324338e50a1be59e04ed9a6
garydblack <gary.black@pnnl.gov>
2023-03-14 17:31:25 -0700

    Reimplemented resilience-method to not use include and exclude lists, but have a deconflict method instead. It averages the values for all the apps other than resilience as a quick and dirty way to handle these.

--- competing-apps/app-deconflictor/resilience-method.py
+++ competing-apps/app-deconflictor/resilience-method.py
@@ -2,15 +2,29 @@
 # Resilience exclusivity deconfliction methodology
 
 class DeconflictionMethod:
-  def __init__(self, AppIncludeList, AppExcludeList,
-               ConflictSetpoints, ConflictTimestamps, Solution):
+
+  def __init__(self, ConflictSetpoints, ConflictTimestamps):
     self.ConflictSetpoints = ConflictSetpoints
     self.ConflictTimestamps = ConflictTimestamps
-    self.Solution = Solution
-
-    AppIncludeList.append('resilience-app')
 
 
   def deconflict():
-    print('*** ERROR: deconflict method should never be called for Resilience Exclusivity deconfliction methodology!\n', flush=True)
+    Solution = {}
+
+    for device in self.ConflictSetpoints:
+      count = 0
+      total = 0.0
+      for app in self.ConflictSetpoints[device]:
+        if app == 'resilience-app':
+          Solution[device] = self.ConflictSetpoints[device][app]
+          count = 0
+          break
+        else:
+          count += 1
+          total += self.ConflictSetpoints[device][app]
+
+      if count > 0:
+        Solution[device] = total/count
+
+    return Solution
 
