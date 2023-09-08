57930c4568ed74fb3a38f995609cde94e2469fe0
garydblack <gary.black@pnnl.gov>
2023-09-08 16:18:13 -0700

    Now have a pulp load shedding app so switched to that name.

--- competing-apps/optimization-apps/run-loadshed.sh
+++ competing-apps/optimization-apps/run-loadshed.sh
@@ -27,9 +27,9 @@ fi
 
 mkdir -p output
 if [ "$1" = "pulp" ] || [ "$1" = "PULP" ] || [ "$1" = "PuLP" ]; then
-  python3 optimization-app-pulp.py load_shed $SIMID "$SIMREQ" $STATE 2>&1 | tee output/loadshed-app.log
+  python3 optimization-app-pulp-loadshed.py load_shed $SIMID "$SIMREQ" $STATE 2>&1 | tee output/loadshed-app.log
 else
-  python3 optimization-app-cvxpy.py load_shed $SIMID "$SIMREQ" $STATE 2>&1 | tee output/loadshed-app.log
+  python3 optimization-app-cvxpy-loadshed.py load_shed $SIMID "$SIMREQ" $STATE 2>&1 | tee output/loadshed-app.log
 fi
-#python3 optimization-app-pulp.py load_shed $SIMID "$SIMREQ" $STATE --outage 56 68 2>&1 | tee output/resilience-app.log
+#python3 optimization-app-pulp-loadshed.py load_shed $SIMID "$SIMREQ" $STATE --outage 56 68 2>&1 | tee output/resilience-app.log
 
