e144815538ebe7e565ccb1afd8d762e91196e581
garydblack <gary.black@pnnl.gov>
2024-06-20 10:04:04 -0700

    First cut at the GridLAB-D version of the pipeline wrapper script.

---
+++ service/deconfliction-pipeline/run-pipeline-gld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# requires at least a reference to the type of simulation to use
+if [ "$#" -lt 2 ]; then
+  echo "Usage: ./run-pipeline.sh <sim_id> <sim_request> [weights_basename]"
+  echo
+  exit
+fi
+
+SIMID=$1
+SIMREQ=$2
+
+WEIGHTS=""
+if [ "$#" -gt 2 ]; then
+  WEIGHTS="--weights=""$3"
+fi
+
+mkdir -p output
+python3 deconfliction-pipeline-gld.py $SIMID "$SIMREQ" $WEIGHTS 2>&1 | tee output/deconfliction-pipeline.log
+
