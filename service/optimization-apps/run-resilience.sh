39d5e8b63dcb6584bc776bc9adfd8c4f78913381
garydblack <gary.black@pnnl.gov>
2025-03-25 15:05:33 -0700

    Run the noreact optimization apps because the reactive power flow version is under development and not yet trusted.

--- service/optimization-apps/run-resilience.sh
+++ service/optimization-apps/run-resilience.sh
@@ -29,7 +29,7 @@ if [ "$OPTLIB" = "pulp" ] || [ "$OPTLIB" = "PuLP" ] || [ "$OPTLIB" = "PULP" ]; t
   python3 optimization-app-pulp.py resilience $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/resilience-app.log
 else
   #python3 optimization-app-cvxpy.py resilience $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/resilience-app.log
-  python3 optimization-app-cvxpy-modular.py resilience $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/resilience-app.log
-  #python3 optimization-app-cvxpy-noreact.py resilience $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/resilience-app.log
+  #python3 optimization-app-cvxpy-modular.py resilience $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/resilience-app.log
+  python3 optimization-app-cvxpy-noreact.py resilience $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/resilience-app.log
 fi
 
