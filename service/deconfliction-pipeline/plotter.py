393842ffc068046b230e23f43e7cd8e8176b0247
garydblack <gary.black@pnnl.gov>
2025-05-29 14:05:53 -0700

    Use plot_data.csv rather than hour_plot_data.csv for creating plots.

--- service/deconfliction-pipeline/plotter.py
+++ service/deconfliction-pipeline/plotter.py
@@ -142,8 +142,7 @@ def _main():
   prefix = 'sim'
 
   hits = 0
-  with open('log/hour_plot_data.csv', 'r') as file:
-  #with open('log/plot_data.csv', 'r') as file:
+  with open('log/plot_data.csv', 'r') as file:
     for line in file:
       tokens = line.split(',')
       if tokens[0] == app:
@@ -185,8 +184,7 @@ def _main():
 
   for iapp in range(len(app_list)):
     hits = 0
-    with open('log/hour_plot_data.csv', 'r') as file:
-    #with open('log/plot_data.csv', 'r') as file:
+    with open('log/plot_data.csv', 'r') as file:
       for line in file:
         tokens = line.split(',')
         if tokens[0] == app_list[iapp]:
