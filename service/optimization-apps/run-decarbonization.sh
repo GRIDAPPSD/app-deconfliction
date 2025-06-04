7d745687d69db4b59405836b1657fc9ae905cae0
garydblack <gary.black@pnnl.gov>
2025-06-04 15:27:39 -0700

    Just noting in a comment that the original cvxpy code can no longer be run without significant updates being made. I do want to keep teh cvxpy code in the repo though because it's the comparison point to the modularized version if I still neded to do that.

--- service/optimization-apps/run-decarbonization.sh
+++ service/optimization-apps/run-decarbonization.sh
@@ -28,8 +28,8 @@ mkdir -p log
 if [ "$OPTLIB" = "pulp" ] || [ "$OPTLIB" = "PuLP" ] || [ "$OPTLIB" = "PULP" ]; then
   python3 optimization-app-pulp.py decarbonization $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/decarbonization-app.log
 else
-  #python3 optimization-app-cvxpy.py decarbonization $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/decarbonization-app.log
   python3 optimization-app-cvxpy-modular.py decarbonization $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/decarbonization-app.log
   #python3 optimization-app-cvxpy-noreact.py decarbonization $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/decarbonization-app.log
+  #NO LONGER RUNNABLE WITHOUT UPDATES python3 optimization-app-cvxpy.py decarbonization $SIMID "$SIMREQ" $INTERVAL 2>&1 | tee log/decarbonization-app.log
 fi
 
