95d23b5639623e806456a2efa348a8388e86a0b8
garydblack <gary.black@pnnl.gov>
2025-06-27 15:15:39 -0700

    Capturing what I hope are the right files to add measurements for the 123apps model. These need to be copied into the CIMHub repo utils directory.

---
+++ service/sim-starter/measurements/insert_measurements_123apps.sh
@@ -0,0 +1,7 @@
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_lines_pq.txt
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_loads.txt
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_machines.txt
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_node_v.txt
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_special.txt
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_switch_i.txt
+python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_xfmr_pq.txt
---
+++ service/sim-starter/measurements/list_measurements_123apps.sh
@@ -0,0 +1,1 @@
+python3 ListMeasureables.py ../cimhubconfig.json ieee123_app_deconfliction _E3D03A27-B988-4D79-BFAB-F9D37FB289F7
