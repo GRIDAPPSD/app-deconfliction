0b46f8f32d8d845f2a3e76551b092ec8e255d5d7
garydblack <gary.black@pnnl.gov>
2025-06-30 15:04:27 -0700

    Can now specify another file besides app_setup.csv for controlling scalability testing runs. Just add the optional name after the 's' code in the run-deconfliction.sh invocation if overriding app_setup.csv as the name.

--- service/optimization-apps/run-scalability.sh
+++ service/optimization-apps/run-scalability.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -lt 2 ]; then
-  echo "Usage: ./run-scalability.sh <sim_id> <sim_request> <line>"
+  echo "Usage: ./run-scalability.sh <sim_id> <sim_request> <line> <app_setup_file>"
   echo
   exit
 fi
@@ -9,11 +9,12 @@ fi
 SIMID=$1
 SIMREQ=$2
 LINENUM=$3
+APPSETUP=$4
 
 mkdir -p log
 
 # extract the specifed line out of app_setup.csv
-LINE=`awk NR==$LINENUM app_setup.csv`
+LINE=`awk NR==$LINENUM $APPSETUP`
 # extract first column to get AppName for log filename
 APPNAME=`echo $LINE | cut -f1 -d,`
 
