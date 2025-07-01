3ee03676f3d2e42fc3634fbe76ea96af5aa618ec
garydblack <gary.black@pnnl.gov>
2025-07-01 11:27:33 -0700

    With help from Monish I figured out the proper way to get measurements for the 123apps model into the platform.

--- service/sim-starter/measurements/insert_measurements_123apps.sh
+++ service/sim-starter/measurements/insert_measurements_123apps.sh
@@ -1,7 +1,5 @@
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_lines_pq.txt
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_loads.txt
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_machines.txt
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_node_v.txt
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_special.txt
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_switch_i.txt
-python3 InsertMeasurements.py ../cimhubconfig.json ieee123_app_deconfliction_xfmr_pq.txt
+#!/bin/bash
+for f in `ls -1 ieee123_app_deconfliction_*.txt`
+do
+  python3 src_python/cimhub/InsertMeasurementsOld.py cimhubconfig.json $f
+done
