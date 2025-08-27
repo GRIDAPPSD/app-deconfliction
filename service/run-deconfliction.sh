5b75644dad9277a2bc60d1cc590ac5f656fe554e
garydblack <gary.black@pnnl.gov>
2025-08-27 12:07:33 -0700

    Allow 5 seconds for other apps to wrap it up just in case they are only a tad behind and therefore don't deserve a forced abort.

--- service/run-deconfliction.sh
+++ service/run-deconfliction.sh
@@ -26,6 +26,7 @@
 cleanup() {
   # note that killing these will mean that as soon as the pipeline is done,
   # everyone will be done (without a choice).
+  sleep 5
   pkill -f "optimization-app-"
   pkill -f "helicsgossbridge"
 }
