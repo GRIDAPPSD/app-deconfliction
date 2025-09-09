c337c10ce64f63386316f1daf26222fec8da1411
garydblack <gary.black@pnnl.gov>
2025-09-09 10:41:27 -0700

    More verbose output for conflict metric information in plot_data.csv. Also switched from min to max cooperation thresholds as I move away from scalability testing to the service paper results.

--- service/deconfliction-pipeline/deconfliction-pipeline.py
+++ service/deconfliction-pipeline/deconfliction-pipeline.py
@@ -1764,11 +1764,11 @@ class DeconflictionPipeline(GridAPPSD):
           if self.rulesStageLastFlag:
             self.pltFile.write(str(self.rulesLastConflictMetric))
             self.pltFile.write(',')
-          self.pltFile.write(str(self.coopResponseCounter))
-          self.pltFile.write(',0.0')
-          self.pltFile.write(',New_Optimization_Setpoints')
+          self.pltFile.write('Responses:' + str(self.coopResponseCounter))
+          self.pltFile.write(',Delta:N/A')
           self.pltFile.write(',Phase:')
           self.pltFile.write(str(self.coopCurrentPhase))
+          self.pltFile.write(',Reason:New_Optimization_Setpoints')
           self.pltFile.write('\n')
 
         # Published IEEE Access Foundational Paper Reference:
@@ -2181,7 +2181,7 @@ class DeconflictionPipeline(GridAPPSD):
             str(self.conflictMetric) + ', responses: ' +
             str(self.coopResponseCounter))
     else:
-      reason = 'Conflict_Metric_Percent_Change'
+      reason = 'Conflict_Metric_Delta'
       prlog('>>> DeconflictSetpoints---threshold YES met for conflict ' +
             'metric % change, concluding COOPERATION with % change: ' +
             str(perConflictDelta) + ', responses: ' +
@@ -2252,13 +2252,13 @@ class DeconflictionPipeline(GridAPPSD):
       if self.rulesStageLastFlag:
         self.pltFile.write(str(self.rulesLastConflictMetric))
         self.pltFile.write(',')
-      self.pltFile.write(str(self.coopResponseCounter))
-      self.pltFile.write(',')
+      self.pltFile.write('Responses:' + str(self.coopResponseCounter))
+      self.pltFile.write(',Delta:')
       self.pltFile.write(str(perConflictDelta))
-      self.pltFile.write(',')
-      self.pltFile.write(reason)
       self.pltFile.write(',Phase:')
       self.pltFile.write(str(self.coopCurrentPhase))
+      self.pltFile.write(',Reason:')
+      self.pltFile.write(reason)
       self.pltFile.write('\n')
 
     # Published IEEE Access Foundational Paper Reference:
@@ -2391,9 +2391,9 @@ class DeconflictionPipeline(GridAPPSD):
     # may impact scalability
 
     # 1) Drive minimum cooperation:
-    self.coopMessagesThreshold = 5
-    self.conflictPercentThreshold = 0.5
-    self.conflictValueThreshold = 0.20
+    #self.coopMessagesThreshold = 5
+    #self.conflictPercentThreshold = 0.5
+    #self.conflictValueThreshold = 0.20
 
     # 2) Drive mid-level cooperation:
     #self.coopMessagesThreshold = 8
@@ -2401,9 +2401,9 @@ class DeconflictionPipeline(GridAPPSD):
     #self.conflictValueThreshold = 0.15
 
     # 3) Drive maximum cooperation:
-    #self.coopMessagesThreshold = 10
-    #self.conflictPercentThreshold = 0.2
-    #self.conflictValueThreshold = 0.10
+    self.coopMessagesThreshold = 10
+    self.conflictPercentThreshold = 0.2
+    self.conflictValueThreshold = 0.10
 
     # multiple cooperation responses are required with a value > 1
     self.coopMinResponses = 2
