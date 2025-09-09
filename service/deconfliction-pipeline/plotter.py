fd23c8cb65dbd43b7318a69e40e83d21836dde1f
garydblack <gary.black@pnnl.gov>
2025-09-09 09:28:04 -0700

    Added commented out app_list setting that is useful for scalability runs.

--- service/deconfliction-pipeline/plotter.py
+++ service/deconfliction-pipeline/plotter.py
@@ -338,6 +338,7 @@ def _main():
 
   #app_list = ['resilience-app', 'max_local-app', 'cvr-app']
   app_list = ['resilience-app', 'max_local-app']
+  #app_list = [] # use this for scalability runs to skip app plotting
   #app_list = ['resilience-app']
   #app_list = ['max_local-app']
   #prefix_list = ['resil', 'max_local', 'cvr']
